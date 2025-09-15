9ee7565c8f1d681da47c3e0567c7f518d3c3184b
Ravi S. Iyer <riyer02@gmail.com>
2025-09-15 23:40:34 +0530

    Shows popup on extension click with Blogger cookie status; Removed hello popup

--- popup.js
+++ popup.js
@@ -1,1 +1,21 @@
-console.log("This is a popup!")
+const blogDomains = [
+  "https://ravisiyer.blogspot.com",
+  "https://ravisiyermisc.blogspot.com",
+  "https://raviswdev.blogspot.com"
+];
+
+const statusList = document.getElementById("statusList");
+
+blogDomains.forEach(domain => {
+  chrome.cookies.get({ url: domain, name: "_ns" }, (cookie) => {
+    const li = document.createElement("li");
+    if (cookie) {
+      li.textContent = `${domain}: ON (_ns=${cookie.value})`;
+      li.className = "on";
+    } else {
+      li.textContent = `${domain}: OFF`;
+      li.className = "off";
+    }
+    statusList.appendChild(li);
+  });
+});
